a7ff73fed304c8f7925c362dabec32cc32f39717
imgurbot12 <imgurbot12@gmail.com>
2020-01-16 22:00:40 -0700

    tabs converted to spaces

--- dhcp/net/iana/__init__.py
+++ dhcp/net/iana/__init__.py
@@ -1,6 +1,6 @@
 import enum
 import struct
-from ..abc import abc
+from .. import abc
 from typing import List
 
 #** Variables **#
@@ -26,43 +26,43 @@ class Arch(enum.Enum):
     EFI_XSCALE        = 8
     EFI_X86_64        = 9
 
-class HWType:
+class HWType(enum.Enum):
     """hardware type as per RFC 2132 and defined by the IANA"""
     Ethernet             = 1
-	ExperimentalEthernet = 2
-	AmateurRadioAX25     = 3
-	ProteonTokenRing     = 4
-	Chaos                = 5
-	IEEE802              = 6
-	ARCNET               = 7
-	Hyperchannel         = 8
-	Lanstar              = 9
-	Autonet              = 10
-	LocalTalk            = 11
-	LocalNet             = 12
-	UltraLink            = 13
-	SMDS                 = 14
-	FrameRelay           = 15
-	ATM                  = 16
-	HDLC                 = 17
-	FibreChannel         = 18
-	ATM2                 = 19
-	SerialLine           = 20
-	ATM3                 = 21
-	MILSTD188220         = 22
-	Metricom             = 23
-	IEEE1394             = 24
-	MAPOS                = 25
-	Twinaxial            = 26
-	EUI64                = 27
-	HIPARP               = 28
-	ISO7816              = 29
-	ARPSec               = 30
-	IPsec                = 31
-	Infiniband           = 32
-	CAI                  = 33
-	WiegandInterface     = 34
-	PureIP               = 35
+    ExperimentalEthernet = 2
+    AmateurRadioAX25     = 3
+    ProteonTokenRing     = 4
+    Chaos                = 5
+    IEEE802              = 6
+    ARCNET               = 7
+    Hyperchannel         = 8
+    Lanstar              = 9
+    Autonet              = 10
+    LocalTalk            = 11
+    LocalNet             = 12
+    UltraLink            = 13
+    SMDS                 = 14
+    FrameRelay           = 15
+    ATM                  = 16
+    HDLC                 = 17
+    FibreChannel         = 18
+    ATM2                 = 19
+    SerialLine           = 20
+    ATM3                 = 21
+    MILSTD188220         = 22
+    Metricom             = 23
+    IEEE1394             = 24
+    MAPOS                = 25
+    Twinaxial            = 26
+    EUI64                = 27
+    HIPARP               = 28
+    ISO7816              = 29
+    ARPSec               = 30
+    IPsec                = 31
+    Infiniband           = 32
+    CAI                  = 33
+    WiegandInterface     = 34
+    PureIP               = 35
 
 class StatusCode:
     """
@@ -71,33 +71,33 @@ class StatusCode:
     https://www.iana.org/assignments/dhcpv6-parameters/dhcpv6-parameters.xhtml#dhcpv6-parameters-5
     """
     # RFC 3315 par. 24..4
-	Success       = 0
-	UnspecFail    = 1
-	NoAddrsAvail  = 2
-	NoBinding     = 3
-	NotOnLink     = 4
-	UseMulticast  = 5
-	NoPrefixAvail = 6
-	# RFC 5007
-	UnknownQueryType = 7
-	MalformedQuery   = 8
-	NotConfigured    = 9
-	NotAllowed       = 10
-	# RFC 5460
-	QueryTerminated = 11
-	# RFC 7653
-	DataMissing          = 12
-	CatchUpComplete      = 13
-	NotSupported         = 14
-	TLSConnectionRefused = 15
-	# RFC 8156
-	AddressInUse               = 16
-	ConfigurationConflict      = 17
-	MissingBindingInformation  = 18
-	OutdatedBindingInformation = 19
-	ServerShuttingDown         = 20
-	DNSUpdateNotSupported      = 21
-	ExcessiveTimeSkew          = 22
+    Success       = 0
+    UnspecFail    = 1
+    NoAddrsAvail  = 2
+    NoBinding     = 3
+    NotOnLink     = 4
+    UseMulticast  = 5
+    NoPrefixAvail = 6
+    # RFC 5007
+    UnknownQueryType = 7
+    MalformedQuery   = 8
+    NotConfigured    = 9
+    NotAllowed       = 10
+    # RFC 5460
+    QueryTerminated = 11
+    # RFC 7653
+    DataMissing          = 12
+    CatchUpComplete      = 13
+    NotSupported         = 14
+    TLSConnectionRefused = 15
+    # RFC 8156
+    AddressInUse               = 16
+    ConfigurationConflict      = 17
+    MissingBindingInformation  = 18
+    OutdatedBindingInformation = 19
+    ServerShuttingDown         = 20
+    DNSUpdateNotSupported      = 21
+    ExcessiveTimeSkew          = 22
 
 #** Classes **#
 
