6f8217f15fe1c5e9c5511be2753b989baa5c11e1
imgurbot12 <imgurbot12@gmail.com>
2020-01-16 21:58:07 -0700

    unified byte base object handler into single object with variable size controls

--- dhcp/abc.py
+++ dhcp/abc.py
@@ -1,6 +1,10 @@
+from typing import List, Union
 
 #** Variables **#
-__all__ = ['ByteOperator']
+__all__ = [
+    'ByteOperator',
+    'ByteObject',
+]
 
 #** Classes **#
 
@@ -15,3 +19,47 @@ class ByteOperator:
     def to_bytes(self) -> bytes:
         """base class method declaration"""
         raise NotImplementedError('must be overwritten!')
+
+class ByteObject(ByteOperator):
+    """basic storage/management tool for both mac-addresses and ip-addresses"""
+
+    def __init__(self, length: int, raw: List[int]):
+        """
+        :param length: length the byte-array is allowed to be
+        :param raw:    list of bytes to be stored in object
+        """
+        # ensure values are valid
+        if len(raw) != length:
+            raise TypeError('invalid byte-count: %d expected: %d' % (
+                len(raw), length))
+        for n, num in enumerate(raw, 1):
+            if num < 0 or num > 255:
+                raise TypeError('%s byte: %d, invalid value: %r' % (
+                    self.__class__.__name__, n, num))
+        # save variables
+        self._length = length
+        self._addr   = bytes(raw)
+
+    def __eq__(self, other: Union['ByteObject', bytes]) -> bool:
+        if isinstance(other, ByteObject):
+            return self._addr == other._addr
+        else:
+            return self._addr == other
+
+    @staticmethod
+    def from_bytes(raw: bytes) -> 'ByteObject':
+        """
+        convert raw byte-string into address object
+
+        :param raw: byte-string being turned into ByteObject
+        :return:    newly generated address object
+        """
+        return ByteObject(len(raw), [c for c in raw])
+
+    def to_bytes(self) -> bytes:
+        """
+        convert byte-object into bytes to pass into buffer
+
+        :return: byte-string
+        """
+        return self._addr
