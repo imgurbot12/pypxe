87cbc7387c6563d5070e169ae1327f115ad76a72
imgurbot12 <imgurbot12@gmail.com>
2020-01-18 14:55:43 -0700

    bug fix in enum collector, added hexdump method for debugging, byte-operator show hex as string

--- dhcp/abc.py
+++ dhcp/abc.py
@@ -18,10 +18,24 @@ def find_enum(enum: enum.Enum, value: Any, default: Any = None) -> Any:
     :param value:   value being retrieved from enum object
     :param default: default value to return only if default is not none
     """
-    if default is None:
-        if value not in enum._value2member_map_:
+    if value not in enum._value2member_map_:
+        if default is None:
             raise ValueError('no such value: %r in enum: %s' % (value, enum))
+        return default
     return enum._value2member_map_[value]
+
+def hexdump(raw: bytes) -> str:
+    """
+    convert raw-bytes into legible hex dump
+
+    :param raw: raw bytes being parsed
+    :return:    hexdump string of bytes
+    """
+    hex = raw.hex()
+    hex = ' '.join(hex[i:i+2] for i in range(0, len(hex), 2))
+    hex = '| '.join(hex[i:i+24] for i in range(0, len(hex), 24))
+    hex = '\n | '.join(hex[i:i+52] for i in range(0, len(hex), 52))
+    return ' | ' + hex + '\n'
 
 #** Classes **#
 
@@ -56,6 +70,9 @@ class ByteObject(ByteOperator):
         # save variables
         self._length = length
         self._addr   = bytes(raw)
+
+    def __str__(self) -> str:
+        return '0x' + self._addr.hex()
 
     def __eq__(self, other: Union['ByteObject', bytes]) -> bool:
         if isinstance(other, ByteObject):
