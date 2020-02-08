2ca89d8ace0f96781cf65b13db81a79b641dc84e
imgurbot12 <imgurbot12@gmail.com>
2020-02-08 19:34:29 +0000

    added options object much like in dhcp library

--- pypxe/tftp/option.py
+++ pypxe/tftp/option.py
@@ -2,7 +2,7 @@
 implementation of TFTP Request Options (RFC 2347-2349)
 """
 import enum
-from typing import List
+from typing import List, Any, Optional
 
 from . import utils
 
@@ -11,7 +11,8 @@ __all__ = [
     'from_bytes',
 
     'Param',
-    'Option'
+    'Option',
+    'Options',
 ]
 
 #** Variables **#
@@ -73,3 +74,60 @@ class Option:
             utils.find_enum(Param, name),
             int(value.decode('utf-8'))
         )
+
+class Options:
+    """dict/list hybrid allowing for quick-lookups of options"""
+
+    def __init__(self, options: List[Option]):
+        """
+        :param options: list of already collection options
+        """
+        self._dict = {opt.name.value:n for n, opt in enumerate(options, 0)}
+        self._list = options
+
+    def __len__(self) -> int:
+        return len(self._list)
+
+    def __iter__(self) -> List['Option']:
+        return iter(self._list)
+
+    def __contains__(self, k: Param) -> bool:
+        return k.value in self._dict
+
+    def __getitem__(self, k: Any) -> 'Option':
+        idx = self._dict[k.value] if isinstance(k, Param) else k
+        return self._list[idx]
+
+    def get(self, k: Param, d: Any = None) -> Optional[int]:
+        """
+        retrieve an item from the list if the paramter exists
+
+        :param k: key being used to retrieve value
+        :param d: default to return if none
+        :return:  option object or default value if not found
+        """
+        return self[k].value if k in self else d
+
+    def blocksize(self) -> int:
+        """
+        retrive block-size option if given in list or default
+
+        :return: requested block-size for transfer
+        """
+        return self.get(Param.BlockSize, 512)
+
+    def timeout(self) -> Optional[int]:
+        """
+        retrieve timeout in seconds if given in list or default
+
+        :return: requested/accepeted timeout value if given or none
+        """
+        return self.get(Param.Timeout, None)
+
+    def totalsize(self) -> Optional[int]:
+        """
+        retrieve total-size option if given in list or default
+
+        :return: requested/accepted total-size value if given or none
+        """
+        return self.get(Param.TotalSize, None)
